c6002f3ef70a5cd9f9c681443ad60dcb75fd2fe2
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-03-12 02:13:08 +0530

    updated

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,34 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  // Get the access token from the request headers
+  const accessToken = req.headers.authorization && req.headers.authorization.split(' ')[1];
+
+  // Check if the access token exists
+  if (!accessToken) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  // Verify the access token
+  jwt.verify(accessToken, 'your_secret_key', (err, user) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // Set user information in the session
+    req.session.user = user;
+
+    // Continue to the next middleware or route handler
+    next();
+  });
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,10 +11,23 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  const bookList = Object.values(books).map(book => {
+    return {
+      title: book.title,
+      author: book.author,
+      isbn: book.isbn,
+    };
+  });
+
+  const response = {
+    message: "List of Books Available in the Shop",
+    books: bookList,
+  };
+  
+  return res.status(200).json(response);
 });
+
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
